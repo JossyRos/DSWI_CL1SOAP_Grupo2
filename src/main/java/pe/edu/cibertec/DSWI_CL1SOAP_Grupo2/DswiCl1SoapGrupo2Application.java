79eee0c4455920c1816a3ed8c14e4d0b54df619a
JossyRos <huarcayajoselyn1@gmail.com>
2024-06-08 19:38:56 -0500

    Pregunta 2

---
+++ src/main/java/pe/edu/cibertec/DSWI_CL1SOAP_Grupo2/DswiCl1SoapGrupo2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWI_CL1SOAP_Grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiCl1SoapGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiCl1SoapGrupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWI_CL1SOAP_Grupo2/DswiCl1SoapGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWI_CL1SOAP_Grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiCl1SoapGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
